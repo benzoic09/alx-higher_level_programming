32524e9a2f1049fa3045ad9d36f39ce4c8b47157
Ben Kimani <benzoic09@gmail.com>
2023-10-05 11:25:09 -0700

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+import sys
+if __name__ == "__main__":
+    num_arg = len(sys.argv) - 1
+
+    if (num_arg = 0:
+            print("0 arguments.")
+    elif num_arg = 1:
+            print("1 argument:")
+    else:
+            print("{} arguments:".format(num_args))
+
+            for i in range(1, num_arg):
+            print(f"{i}: {args_list[i]}")
