f12ff71d29b0e72be395826d2e8b599064ff75f3
Ben Kimani <benzoic09@gmail.com>
2023-10-23 12:31:02 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValuError:
+    except ValuError as e:
+         print("Exception: {}".format(e), file=sys.stderr)
         return False
