30ce8c021b5d38b4fc78650aac7457ba638dbae0
Ben Kimani <benzoic09@gmail.com>
2023-10-23 05:29:24 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,10 @@ def safe_print_list(my_list=[], x=0):
     try:
         for element in my_list:
             if element_printed < x:
-                print(element, end=' ')
+                print(element, end='')
                 element_printed += 1
                 print()
     except IndexError:
         return element
-    return (element_printed)
+    else:
+        return (element_printed)
