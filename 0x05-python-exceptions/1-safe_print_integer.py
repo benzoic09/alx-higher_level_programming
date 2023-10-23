72e06d5939bfaa6545ede5e7c14ad6100eed19bb
Ben Kimani <benzoic09@gmail.com>
2023-10-23 06:37:22 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@ def safe_print_integer(value):
      try:
         formatted_value = "{:d}".format(int(value))
         print(formatted_value)
-        print()  # Add a new line
+        print() 
         return True
     except (ValueError, TypeError):
         return False
