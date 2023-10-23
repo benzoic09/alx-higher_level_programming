47dcec6ccdda15038515a5eb35c728b0589efb63
Ben Kimani <benzoic09@gmail.com>
2023-10-23 05:49:55 -0700

    0x05-python-exceptions

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    interger_value = int(value)
+    print("{:d}".format(interger_value))
+    print()
+    return True
+expection(ValueError, TypeError):
+    return False
