ea077f223a3308cd0f6fd39ebdab469bf215749c
Ben Kimani <benzoic09@gmail.com>
2023-10-24 12:38:38 -0700

    0x06-python-classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,16 +5,20 @@ Square module
 
 
 class Square:
-    """class square"""
+    """ class square"""
     def __init__(self, size=0):
         self.__size = size
 
+    @property
     def size(self):
-        if type(size) != int:
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        return self.__size
 
     def area(self):
         return self.__size * self.__size
