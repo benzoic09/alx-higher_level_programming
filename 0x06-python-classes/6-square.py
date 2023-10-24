6049fa56e22cc7e0085d93657da4bf30b32f3441
Ben Kimani <benzoic09@gmail.com>
2023-10-24 15:01:27 -0700

    0x06-python-classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,12 +15,12 @@ class Square():
         return self.__size
 
     @size.setter
-    def size(self, value):
+    def size(self, size):
         if type(value) != int:
             raise TypeError("size must be an integer")
-        if value < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self.__size = size
 
     @property
     def position(self):
