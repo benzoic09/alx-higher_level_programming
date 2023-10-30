38915fb1e9acee29d284b39a38c9673fbd2e024a
Ben Kimani <benzoic09@gmail.com>
2023-10-30 12:00:38 -0700

    0x08-python-more_classes

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,7 +7,7 @@ class rectangle
 class Rectangle:
     """ rectangle class"""
     number_of_instances = 0
-    print_symbol = "#"
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         self.width = width
