95b9bb52591f94ee0bdea213abd516e0b2f8f93b
Ben Kimani <benzoic09@gmail.com>
2023-11-01 14:15:52 -0700

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,6 +5,7 @@ say my name function
 
 
 def say_my_name(first_name, last_name=""):
+    """say my name"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
