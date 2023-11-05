06522f521d65bb86aba1a42ce21cdf512e6937a0
Ben Kimani <benzoic09@gmail.com>
2023-11-05 03:02:46 -0800

    0x09-python-everything_is_object

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,11 @@ add 2 numbers
 
 
 def add_integer(a, b=98):
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+    """ "Return the sum of two integers or floats as an integer."""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
     return int(a) + int(b)
