da0ef619d4ab8ec43a1c6a43244a6e99a083d427
Ben Kimani <benzoic09@gmail.com>
2023-11-06 13:27:44 -0800

    0x0A-python-inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -10,9 +10,9 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """ derived class"""
     def __init__(self, size):
-         self.integer_validator("size", size)
-          super().__init__(size, size)
-         self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
 
 
     def __str__(self):
