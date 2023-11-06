b13ad6ac4ab0d3c92f7e0614ccf802d1b423ab21
Ben Kimani <benzoic09@gmail.com>
2023-11-06 13:23:37 -0800

    0x0A-python-inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -10,12 +10,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """ derived class"""
     def __init__(self, size):
-         self.integer_validator("size", size)
          super().__init__(size, size)
+         self.integer_validator("size", size)
          self.__size = size
 
-    def area(self):
-        return self.__size * self.__size
 
     def __str__(self):
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
