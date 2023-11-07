e15f5d74a226696af94c92998fa0ebc2fcc9f832
Ben Kimani <benzoic09@gmail.com>
2023-11-07 05:20:42 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,7 @@ opens and prints a file
 
 
 def read_file(filename=""):
+    """ prints file content"""
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')
