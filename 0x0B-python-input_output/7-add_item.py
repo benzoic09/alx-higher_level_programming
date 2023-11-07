0202b471ab9f74b191fc897c5686e6257b98f4fd
Ben Kimani <benzoic09@gmail.com>
2023-11-07 10:52:13 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,16 +5,17 @@ and then save them to a file
 """
 import json
 import sys
-import os.path
 from load_from_json_file import load_from_json_file
 from save_to_json_file import save_to_json_file
 
 
-filename = "add_item.json"
+filename = "add_item.json"i
+
 if os.path.isfile(filename):
     obj = load_from_json_file(filename)
 else:
     obj = []
 
-obj.extend(sys.argv[1:])
+arg = sys.argv[1:]
+obj.extend(arg)
 save_to_json_file(obj, filename)
