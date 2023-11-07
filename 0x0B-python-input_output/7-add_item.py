b066cda33d97b5f7ffa8ff3640d03c7abe9b9b49
Ben Kimani <benzoic09@gmail.com>
2023-11-07 09:50:11 -0800

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+script that adds all arguments to a Python list, 
+and then save them to a file
+"""
+import json
+
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+if os.
