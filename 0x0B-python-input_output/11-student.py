c0261a4afa130ca04854ddf67f17a2fac24442f1
Ben Kimani <benzoic09@gmail.com>
2023-11-08 10:16:21 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- class Student that defines a student by: 
+ class Student that defines a student by:
  (based on 10-student.py)
  """
 
@@ -15,7 +15,8 @@ class Student:
         def to_json(self, attrs=None):
             if attrs is None:
                 return self.__dict__
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
 
         def reload_from_json(self, json):
             for key, value in json.items():
