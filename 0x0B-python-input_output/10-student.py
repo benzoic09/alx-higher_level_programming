7c13e77365877041ff6bd8e8fe74a1304f7c7702
Ben Kimani <benzoic09@gmail.com>
2023-11-08 08:32:30 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -4,7 +4,7 @@ class Student that defines a student
 """
 
 
-class Student:
+class Studenit:
     """ student class"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
@@ -12,7 +12,7 @@ class Student:
         self.age = age
 
     def to_json(self):
-         if attrs is None:
+        if attrs is None:
             return self.__dict__
         return {key: value for (key, value) in self.__dict__.items()
                 if key in attrs}
