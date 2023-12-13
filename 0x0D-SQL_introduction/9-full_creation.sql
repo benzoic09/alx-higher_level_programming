c1f90a19d34c165182a2cfdbff79f60b8b48d1af
Ben Kimani <benzoic09@gmail.com>
2023-12-13 11:42:13 -0800

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,4 @@
--- script that creates a table second_table in the database 
-	-- hbtn_0c_0 in your MySQL server and add multiples rows.
+-- script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
 INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
