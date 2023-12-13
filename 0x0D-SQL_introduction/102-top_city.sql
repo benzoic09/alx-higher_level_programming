8f4161a1e82d98bea05555bc8e47083c41f3995e
Ben Kimani <benzoic09@gmail.com>
2023-12-13 13:13:23 -0800

    0x0D-SQL_introduction

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,5 @@
+-- script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+SELECT city,AVG(value) as Avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY Avg_temp DESC
