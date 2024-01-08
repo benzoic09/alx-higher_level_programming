79614896cfb891e10a5e1b18fc07b1f166bfadb4
Ben Kimani <benzoic09@gmail.com>
2024-01-08 06:12:07 -0800

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const Args = process.argv.length - 2;
-if (Args === 0) 
-  console.log('No argument');
- else if (Args === 1) 
+if (Args === 0)
+	console.log('No argument');
+else if (Args === 1) 
   console.log('Argument found');
- else 
-  console.log('Arguments found');
+else 
+	console.log('Arguments found');
