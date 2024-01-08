7f51c5f4c23908761daf4eb56cb5396d1e7824d3
Ben Kimani <benzoic09@gmail.com>
2024-01-08 12:59:18 -0800

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
 function add (a, b) {
-  return a + b;
+  return (a + b);
 }
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
+const a = process.argv[2];
+const b = process.argv[3];
 
-const num1 = Number(arg1);
-const num2 = Number(arg2);
-
-if (!isNaN(num1) && !isNaN(num2)) {
-  console.log(add(num1, num2));
-} else {
-  console.log('Invalid input. Please provide two integers.');
-}
+console.log(add(parseInt(a), parseInt(b)));
