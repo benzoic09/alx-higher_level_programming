b37f9de55a3107f3fa18cb608afee0fad2bce409
Ben Kimani <benzoic09@gmail.com>
2024-01-08 11:44:36 -0800

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,8 +4,8 @@ const num = Number(arg);
 
 if (!isNaN(num)) {
   for (let i = 0; i < num; i++) {
-  console.log('C is fun');
-} 
-}  else {
+    console.log('C is fun');
+  }
+} else {
   console.log('Missing number of occurrences');
 }
