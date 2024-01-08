5953c1ca5ad8af6735062453b0beb1adc189c722
Ben <benzoic09@gmail.com>
2024-01-08 23:28:57 +0300

    0x12-javascript-warm_up

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+function add(a, b) {
+    return a + b;
+}
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+const num1 = Number(arg1);
+const num2 = Number(arg2);
+
+if (!isNaN(nun1) && !isNaN(num2)) {
+    console.log(add(num1, num2));
+} else {
+    console.log('Invalid input. Please provide two integers.');
+}
+
