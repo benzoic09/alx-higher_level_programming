e98e5eadfebff0f802365a8474dcfe802021659e
Ben Kimani <benzoic09@gmail.com>
2024-01-08 13:16:59 -0800

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const args = process.argv.slice(2).map(Number);
-
-if (args.includes(12)) {
-  const updatedArgs = args.map(num => num === 12 ? 89 : num);
-  console.log(updatedArgs);
-} else {
-  console.log(args);
-}
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
