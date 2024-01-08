17f996032e80481dc4a56633de1b0351b73b785e
Ben Kimani <benzoic09@gmail.com>
2024-01-08 08:23:01 -0800

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const Arg = process.argv[2];
-if (Arg === undefined)
-	console.log("No argument");
-else
-	console.log(Arg);
+if (Arg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(Arg);
+}
