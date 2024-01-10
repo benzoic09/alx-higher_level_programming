807aedd48c8d54d7dadff6cdf049ac809e143dce
ben <benzoic09@gmail.com>
2024-01-10 09:25:25 -0800

    task 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,40 +1,6 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      return this;
-    } else {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    if (!this.width || !this.height) {
-      console.log('Empty object');
-    } else {
-      for (let count = 0; count < this.height; count++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
-  }
-
-  rotate () {
-    if (this.width && this.height) {
-      [this.width, this.height] = [this.height, this.width];
-    }
-  }
-
-  double () {
-    if (this.width && this.height) {
-      this.width *= 2;
-      this.height *= 2;
-    }
-  }
-
-class Square extends Rectangle {
+module.exports = class Square extends require('./4-rectangle.js') {
   constructor (size) {
     super(size, size);
   }
-}
 };
