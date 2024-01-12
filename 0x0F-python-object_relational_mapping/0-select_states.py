2cacdace32ff5ce2155dbbd66ad7961a6b25c0eb
benzoic09 <104141553+benzoic09@users.noreply.github.com>
2024-01-12 14:25:28 +0300

    fix number zero

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,21 +3,20 @@
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(user=sys.argv[1],
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3],
                          host='localhost',
                          port=3306)
 
-cur = db.cursor()
+    cur = db.cursor()
 
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-states = cur.fetchall()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cur.fetchall()
 
-for state in states:
+    for state in states:
         print(state)
 
-cur.close()
-db.close()
-
-if __name__ == "__main__":
+    cur.close()
+    db.close()
