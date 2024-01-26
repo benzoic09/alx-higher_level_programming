871fcd29bbb776b526a5b5c1c75e92e319179cf3
ben <benzoic09@gmail.com>
2024-01-26 04:48:57 -0800

    task zero

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type:", type(content))
         print("\t- content:", content.decode('utf-8'))
-        print("\t- utf8 content:", content.decode("utf-8"))
+        print("\t- utf8 content:", content.decode('utf-8'))
