98739f15d4291bc95232c7c5f7d8dbd0f77c7b15
ben <benzoic09@gmail.com>
2024-01-26 08:01:03 -0800

    mod task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the 
+"""Python script that takes in a URL, sends a request to the
 URL and displays the body of the response (decoded in utf-8).
 """
 
@@ -11,11 +11,10 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = urllib.parse.urlencode({'email':email}).encode('utf-8')
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     req = urllib.request.Request(url, data=data)
 
     with urllib.request.urlopen(req) as response:
         body = response.read().decode('utf-8')
 
         print(body)
-
