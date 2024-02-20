2ee97b00f5e997e74076a1d2e91ba5c5c28a6dc1
ben <benzoic09@gmail.com>
2024-02-20 13:06:30 -0800

    task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, function (err, response, body) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedTasksByUser = {};
+
+  todos.forEach(todo => {
+    if (todo.completed) {
+      const userId = todo.userId;
+      completedTasksByUser[userId] = (completedTasksByUser[userId] || 0) + 1;
+    }
+  });
+
+  for (const userId in completedTasksByUser) {
+    console.log(`${userId}: ${completedTasksByUser[userId]}`);
+  }
+});
