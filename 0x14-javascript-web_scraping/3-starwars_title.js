3ee6132d6e7796bb3a7af3ca705c1f42fad50d6a
ben <benzoic09@gmail.com>
2024-02-20 04:51:47 -0800

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -9,7 +9,7 @@ request.get(apiurl, function (err, response, body) {
   if (err) {
     console.error(err);
   } else {
-    if (response.statusCode == 200) {
+    if (response.statusCode === 200) {
       const movie = JSON.parse(body);
       console.log(movie.title);
     }
