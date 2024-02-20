670e7d12c1b8c050e69e0cb801ca7fc11aa6bb0b
ben <benzoic09@gmail.com>
2024-02-20 09:21:15 -0800

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -9,10 +9,11 @@ const filePath = process.argv[3];
 request.get(url, function (err, response, body) {
   if (err) {
     console.error(err);
-    return;
+  } else {
+    fs.writeFile(filePath, body, 'utf8', function (err, data) {
+      if (err) {
+        console.error(err);
+      }
+    });
   }
-
-  fs.writeFile(filePath, body, 'utf8', function (err) {
-    console.log(`${filePath}`);
-  });
 });
